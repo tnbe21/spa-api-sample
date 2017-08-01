9f8a43c6ddacb4eed2c892ed1bf3103c554c2af0
tanabe_taichi <tanabe_taichi@cyberagent.co.jp>
2017-08-02 01:33:09 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const _ = require('lodash');
+const express = require('express');
+const async = require('neo-async');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const MONGO_URL = 'mongodb://localhost:27017/todo';
+
+const mongoConnect = (callback) => {
+  MongoClient.connect(MONGO_URL, callback);
+};
+
+app.get('/status', (req, res) => {
+  res.ok('alive');
+});
+
+app.get('/todo/find', (req, res) => {
+  async.angelFall([
+    mongoConnect,
+    (db, next) => {
+      db.collection('todo').find({}, next);
+    }
+  ], (err, todos) => {
+    if (err)
+      return res.status(500).json(err);
+    res.json(todos);
+  });
+});
+
+app.post('/todo/save', (req, res) => {
+  async.angelFall([
+    mongoConnect,
+    (db, next) => {
+      const todo = _.pick(req.params, ['_id', 'body']);
+      db.collection('todo').update(todo, {w: 1}, next);
+    }
+  ], (err) => {
+    if (err)
+      return res.status(500).json(err);
+    res.ok();
+  });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`app listening on port ${PORT}!`)
+});
