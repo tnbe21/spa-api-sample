6e01fa91c1f2f7392e7023ee8b758b448b2d0db1
tnbe21 <whpbj809@gmail.com>
2017-08-07 09:36:05 +0000

    fix

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const mongoConnect = (callback) => {
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://52.193.135.71');
+console.log('set header');
   next();
 });
 
@@ -19,28 +20,36 @@ app.get('/status', (req, res) => {
 });
 
 app.get('/todo/find', (req, res) => {
+  let db = {};
   async.angelFall([
-    mongoConnect,
-    (db, next) => {
-      db.collection('todo').find({}, next);
+    (next) => { mongoConnect(next); },
+    (_db, next) => {
+      db = _db;
+      db.collection('todo').find({}).toArray(next);
     }
   ], (err, todos) => {
     if (err)
       return res.status(500).json(err);
+
+    db.close();
+
     res.json(todos);
   });
 });
 
 app.post('/todo/save', (req, res) => {
+  let db = {};
   async.angelFall([
-    mongoConnect,
-    (db, next) => {
+    (next) => { mongoConnect(next); },
+    (_db, next) => {
+      db = _db;
       const todo = _.pick(req.params, ['_id', 'body']);
       db.collection('todo').update(todo, {w: 1}, next);
     }
   ], (err) => {
     if (err)
       return res.status(500).json(err);
+    db.close();
     res.ok();
   });
 });
